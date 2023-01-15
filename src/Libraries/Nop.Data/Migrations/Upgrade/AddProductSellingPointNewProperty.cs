e43bf1911b2fd9371fea4bec0b9573acbe4b8524
Hamzeh Hanandeh <h.hananda@outlook.com>
2023-01-15 21:04:30 +0300

    NopMigration: Add product "SellingPoint" new property

---
+++ src/Libraries/Nop.Data/Migrations/Upgrade/AddProductSellingPointNewProperty.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Nop.Core.Domain.Catalog;
+using Nop.Data.Migrations;
+
+[NopMigration("2023-01-15 00:00:00", "Product. Add selling point", UpdateMigrationType.Data, MigrationProcessType.Update)]
+public class AddProductSellingPointNewProperty : AutoReversingMigration
+{
+    /// <summary>Collect the UP migration expressions</summary>
+    public override void Up()
+    {
+        Create.Column(nameof(Product.SellingPoint))
+        .OnTable(nameof(Product))
+        .AsString(int.MaxValue)
+        .Nullable();
+    }
+}
